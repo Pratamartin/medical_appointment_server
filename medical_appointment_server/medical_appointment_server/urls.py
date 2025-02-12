f99891b89b76b3ad75fd055a7bc7e354c5ac7570
Martinho Prata <martinhoprata95@gmail.com>
2025-02-11 22:18:01 -0400

    feat(view): Add welcome view

--- medical_appointment_server/medical_appointment_server/urls.py
+++ medical_appointment_server/medical_appointment_server/urls.py
@@ -18,8 +18,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+
+def welcome_view(request):
+    return HttpResponse("Welcome to Medical Appointment Server")
+
 
 urlpatterns = [
+    path('', welcome_view, name='welcome'),
     path('admin/', admin.site.urls),
     path('api/', include('medical_appointment_app.urls')),
 ]
